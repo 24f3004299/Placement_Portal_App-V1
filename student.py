5fba2bc9203fe368dae551db66d17a54cf04a883
24f3004299 <24f3004299@ds.study.iitm.ac.in>
2026-02-20 18:01:33 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,6 @@
 #from app import app
 from flask import render_template, url_for, request, redirect
-from db import *
+from model import *
 from flask import Blueprint,session
 from flask_login import login_required
 stu=Blueprint('student','__name__')
@@ -11,4 +11,5 @@ stu=Blueprint('student','__name__')
 #@login_required
 def student(idi):
     return  render_template("student.html",idi=idi)
+
    
